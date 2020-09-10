4588d1faa631d1eeba06841bb84a53a5ebf7be80
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-09-11 00:36:48 +0100

    before start page

---
+++ src/pages/beforeStart.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react"
+import { Link, graphql, useStaticQuery } from "gatsby"
+
+import firebase from "gatsby-plugin-firebase"
+import Survey from "../components/Survey"
+import { logIn, startSurvey } from "../components/Survey/service"
+import { Layout, Header } from "../components"
+
+export default () => {
+  const [ready, setReady] = useState(false)
+  useEffect(() => {
+    if (!firebase) return
+    logIn()
+    return firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+        setReady(true)
+      }
+    })
+  }, [firebase])
+
+  return (
+    <Layout>
+      <div className="survey">
+        <div className="container">
+          <Header>
+            <p>Before You Start</p>
+            <p> Play some music</p>
+          </Header>
+          <div className="rules">
+            <h1> State of dev in morocco 2020</h1>
+            <p>
+              * we care about privacy that why we are using an anonymous
+              authentication system to make sure we have 1 submission per user{" "}
+            </p>
+            <p>
+              * Question with (*) is required and (multiple) are multiple
+              answers allowed and You can skip question using skip button if its
+              not required and the question scope not include you{" "}
+            </p>
+            <p>
+              * Try to be honest answering questions As this will reflect our
+              final result.
+            </p>
+            <p>
+              * Please do not refresh the page while submitting your answers{" "}
+            </p>
+            <p>You can play some music during the survey</p>
+            <p>....</p>
+            <p> </p>
+            <br />
+            <Link className="primary" to="/start">
+              Let’s do it
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
