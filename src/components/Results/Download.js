89f5d693a9d3d990f3eb15d6903db3ca19032135
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-12-12 15:26:35 +0100

    add intro content & fix styling

--- src/components/Results/Download.js
+++ src/components/Results/Download.js
@@ -18,36 +18,7 @@ export const Download = () => {
             target="_blank"
             href="https://github.com/DevC-Casa/stateofdev.ma/blob/results_prview/results/2020/state-od-dev-ma-2020.zip?raw=true"
           >
-            <svg
-              style={{ marginRight: 10 }}
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M12.1211 15.436L12.1211 3.39502"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M15.0371 12.5083L12.1211 15.4363L9.20511 12.5083"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-              <path
-                d="M16.7541 8.12793H17.6871C19.7221 8.12793 21.3711 9.77693 21.3711 11.8129V16.6969C21.3711 18.7269 19.7261 20.3719 17.6961 20.3719L6.55609 20.3719C4.52109 20.3719 2.87109 18.7219 2.87109 16.6869V11.8019C2.87109 9.77293 4.51709 8.12793 6.54609 8.12793L7.48809 8.12793"
-                stroke="white"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </svg>
+            <DownloadIcon />
             Download Survey Results
           </a>
           <Link to="/playground" className="outline">
@@ -56,5 +27,34 @@ export const Download = () => {
         </div>
       </div>
     </div>
+  )
+}
+
+function DownloadIcon(props) {
+  return (
+    <svg
+      style={{ marginRight: 10 }}
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M12.121 15.436V3.395M15.037 12.508l-2.916 2.928-2.916-2.928"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M16.754 8.128h.933a3.684 3.684 0 013.684 3.685v4.884a3.675 3.675 0 01-3.675 3.675H6.556a3.685 3.685 0 01-3.685-3.685v-4.885a3.675 3.675 0 013.675-3.674h.942"
+        stroke="#fff"
+        strokeWidth={2}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
   )
 }
