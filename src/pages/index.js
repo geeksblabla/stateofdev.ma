c29757208cc4dee3f2e9ca397ee3fca65a1beb02
Youssouf El Azizi <youssoufelazizi@gmail.com>
2020-12-13 17:23:31 +0100

    fix seo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from "react"
 import Results from "../components/Results/index"
+import { Layout } from "../components"
 
 export default () => {
-  return <Results />
+  return (
+    <Layout>
+      <Results />
+    </Layout>
+  )
 }
