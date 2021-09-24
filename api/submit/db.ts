bea190f36316971c6c746bba8ba6c128f18fb319
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-09-24 16:36:19 +0100

    feat: migrate survey submition to server function instead of firebase client

---
+++ api/submit/db.ts
@@ -0,0 +1,48 @@
+import * as admin from "firebase-admin"
+import fetch from "node-fetch"
+
+const getFirebase = () => {
+  const firebasePrivateKey = process.env.FIREBASE_PRIVATE_KEY
+  if (!admin.apps.length) {
+    admin.initializeApp({
+      credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        // https://stackoverflow.com/a/41044630/1332513
+        privateKey: firebasePrivateKey?.replace(/\\n/g, "\n"),
+      }),
+    })
+  }
+  return admin
+}
+
+const Results = getFirebase().firestore().collection("/results")
+
+export const saveAnswer = (userId: string, data: any) => {
+  return Results.doc(userId).set(data, { merge: true })
+}
+
+export const getUser = (token: string) => {
+  return admin.auth().verifyIdToken(token)
+}
+
+//https://developers.google.com/recaptcha/docs/verify
+export const verifyRecaptcha = async (recaptcha_token: string) => {
+  const url = "https://www.google.com/recaptcha/api/siteverify"
+  const params = {
+    secret: process.env.RECAPTCHA_SECRET_KEY,
+    response: recaptcha_token,
+  }
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: `secret=${params.secret}&response=${params.response}`,
+    })
+    const res = await response.json() // parses JSON response into native JavaScript objects
+    return res
+  } catch (error) {
+    throw new Error("error verifying recaptcha ")
+  }
+}
