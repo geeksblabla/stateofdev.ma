02ecdd0df2c45eab7ec0d22cce70f3e44d2d54b9
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-09-24 21:11:28 +0100

    fix firebase initialization on netlify function

--- api/submit/db.ts
+++ api/submit/db.ts
@@ -6,7 +6,7 @@ const getFirebase = () => {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
+        projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // https://stackoverflow.com/a/41044630/1332513
         privateKey: firebasePrivateKey?.replace(/\\n/g, "\n"),
@@ -16,10 +16,10 @@ const getFirebase = () => {
   return admin
 }
 
-const Results = getFirebase().firestore().collection("/results")
+const getResults = () => getFirebase().firestore().collection("/results")
 
 export const saveAnswer = (userId: string, data: any) => {
-  return Results.doc(userId).set(data, { merge: true })
+  return getResults().doc(userId).set(data, { merge: true })
 }
 
 export const getUser = (token: string) => {
