3c16e353ba131721526ab6bed5788b2c603fcb8c
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2021-12-19 17:29:11 +0100

    fix testing script

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,4 +1,5 @@
 const babelOptions = {
   presets: ["babel-preset-gatsby"],
 }
-module.exports = require("babel-jest").createTransformer(babelOptions)
+
+module.exports = require("babel-jest").default.createTransformer(babelOptions)
