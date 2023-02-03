826529b84bc4fb99755c1a2240527d4e4789751b
Azi Hassan <azihassan@users.noreply.github.com>
2023-02-03 18:56:04 +0100

    fix: remove duplicated header in 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,6 @@ export default function Index() {
     <Layout>
       <div className="thanks">
         <div className="container">
-          <Header />
           <div className="main">
             <img src="/images/tarbouch.png" />
             <h1> 404 {`:)`} </h1>
