4aa34d4b91591194b5073f9ea57b5c6de077045f
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-08-29 09:42:51 +0100

    feat: setup markdown config + logic to handle multiple section for reports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,17 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
-
 import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()],
+  integrations: [tailwind(), icon(), mdx(), react()],
   experimental: {
-    actions: true,
+    actions: true
   },
   trailingSlash: "never",
   output: "server",
-  adapter: cloudflare(),
+  adapter: cloudflare()
 });
