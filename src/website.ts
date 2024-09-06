c603866acb49a697ea5be5df016d625fc0b25336
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-09-06 12:34:09 +0100

    feat: layout seo

---
+++ src/website.ts
@@ -0,0 +1,32 @@
+export const website = {
+  pathPrefix: "/",
+
+  siteTitle: "State Of Dev In Morocco",
+  siteTitleAlt: "State Of Dev In Morocco",
+  siteUrl: "https://stateofdev.ma",
+  siteLanguage: "en",
+  siteLogo: "images/logo.png",
+  banner: "images/banner.png",
+  siteDescription:
+    "Participate and let  us know what working in tech really looks like in Morocco 🇲🇦", // Author for schemaORGJSONLD
+  organization: "GeeksBlaBla",
+  author: "GeeksBlaBla",
+  userTwitter: "@geeksblabla",
+  ogSiteName: "StateOfDevMa",
+  ogLanguage: "en_US",
+  googleAnalyticsID: "",
+
+  // Manifest and Progress color
+  themeColor: "#5348FF",
+  backgroundColor: "#2b2e3c",
+
+  // Social Media
+  twitter: "https://twitter.com/geeksblabla",
+  twitterHandle: "@geeksblabla",
+  github: "https://github.com/geeksblabla",
+  linkedin: "https://www.linkedin.com/company/geeksblabla-community",
+  facebook: "https://www.facebook.com/geeksblabla",
+  instagram: "https://www.instagram.com/geeksblabla",
+  youtube: "https://www.youtube.com/channel/UCW2WV7NKU0WPyuv4YoNSqBA",
+  repoGithub: "https://github.com/geeksblabla/stateofdev.ma",
+};
