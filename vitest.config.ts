3d84c823de17158803aff3949d5fb1d7bd67c0df
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-09-16 10:53:43 +0100

    feat: add test reports with github action

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,12 @@ import { getViteConfig } from "astro/config";
 
 export default getViteConfig({
   test: {
-    exclude: ["node_modules", "old-website"],
+    exclude: ["node_modules"],
+    coverage: {
+      // you can include other reporters, but 'json-summary' is required, json is recommended
+      reporter: ["text", "json-summary", "json"],
+      // If you want a coverage reports even if your tests are failing, include the reportOnFailure option
+      reportOnFailure: true,
+    },
   },
 });
