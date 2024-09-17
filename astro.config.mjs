cc1353c21b9f4d81d752b97977640bfb087c44d6
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-09-17 12:52:13 +0100

    chore: switch to netlify for hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 
 import react from "@astrojs/react";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon(), mdx(), react()],
   trailingSlash: "never",
   output: "hybrid",
-  adapter: cloudflare({
-    platformProxy: {
-      enabled: true,
-      configPath: "wrangler.toml",
-    },
-  }),
-  vite: {
-    ssr: {
-      external: ["url", "path", "fs", "crypto", "http", "https", "os","http2","events", "zlib", "node:stream","node:util", "node:events","stream", "util", "buffer", "node:buffer"],
-    },
-  },
+  adapter: netlify()
 });
