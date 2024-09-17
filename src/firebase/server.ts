91aa8db4878cea7fb51a5c9e24c1fce281d2f2e3
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-09-17 13:30:56 +0100

    feat: fix issue with firebase credentials

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -17,12 +17,6 @@ const serviceAccount = {
 };
 
 const initApp = () => {
-  if (import.meta.env.PROD) {
-    console.info("PROD env detected. Using default service account.");
-    // Use default config in firebase functions. Should be already injected in the server by Firebase.
-    return initializeApp();
-  }
-  console.info("Loading service account from env.");
   return initializeApp({
     credential: cert(serviceAccount as ServiceAccount)
   });
