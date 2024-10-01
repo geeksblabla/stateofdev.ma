46d0dce84c1f31114cb8fcffa1bf5c7abc2c8c65
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-10-01 10:45:46 +0100

    fix: fix backup script private key 🤯

--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -24,8 +24,8 @@ if (import.meta.env?.FIREBASE_PROJECT_ID) {
   serviceAccount = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID?.replace(/\\n/g, "\n"), // not sure why, but the private key is not working correctly without this in github actions
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"), // not sure why, but the private key is not working correctly without this in github actions
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
