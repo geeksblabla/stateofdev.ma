e0da2938d35cef961c1699a033e83f85743ddd16
Youssouf EL Azizi <youssoufelazizi@gmail.com>
2024-12-31 21:30:21 +0100

    feat: close survey

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,11 @@ export default defineConfig({
   trailingSlash: "never",
   output: "hybrid",
   adapter: netlify(),
+  redirects: {
+    "/before-start": "/",
+    "/survey": "/",
+    "/thanks": "/"
+  },
   vite: {
     plugins: [yaml()]
   }
